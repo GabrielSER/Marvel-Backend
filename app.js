9840b5324e18205329b134ac073940412a06be32
Marco Acosta <powerengine98@gmail.com>
2024-02-27 23:50:27 -0500

    Added static serve handler

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ require('dotenv').config()
 var fs = require('fs')
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
+
 const dbConnect = require('./config/mongo')
 
 app.use(cors())
 app.use(express.json())
 
 const {
-    PORT
+    PORT = 5000,
+    SERVE_PATH = 'public'
 } = process.env
 
 const attributeController = require('./controllers/attributes')
-// form.set('specialSkills', undefined, {strict: false} )
 
 app.listen(PORT, async () => {
     console.log(fs.readFileSync('./assets/banner.txt').toString('utf-8'))
@@ -24,11 +26,17 @@ app.listen(PORT, async () => {
     } catch (error) {
         console.error('Database connection error', error)
     }
-
-    app.use('/api', require('./routes'))
     
     console.log('\nServer started!\n')
     
     await attributeController.bootstrap()
-})
+}) 
 
+app.use('/api', require('./routes'))
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, SERVE_PATH)))
+
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, SERVE_PATH, 'index.html'))
+})
